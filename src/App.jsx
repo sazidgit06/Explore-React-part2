5f5d4c089e240cb1a69706fedc97ac7d2ad117ea
Sazid <sazidtec06@gmail.com>
2025-05-15 01:20:23 +0600

    github added and initializing the project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h3>React core concept part2</h3>
+      
+    </>
+  )
+}
+
+export default App
